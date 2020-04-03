fd672279f37436b6a1a6c3862c41102530553cb3
matovaya <62945293+matovaya@users.noreply.github.com>
2020-04-03 18:25:18 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -15,12 +15,12 @@ def browser(request):
     if browser_name == "chrome":
         options = Options()
         options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-        print("\nstart chrome browser for test..")
+        print("\nstart chrome browser for fun..")
         browser = webdriver.Chrome(options=options)
     elif browser_name == "firefox":
         fp = webdriver.FirefoxProfile()
         fp.set_preference("intl.accept_languages", user_language)
-        print("\nstart firefox browser for test..")
+        print("\nstart firefox browser for fun..")
         browser = webdriver.Firefox(firefox_profile=fp)
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
